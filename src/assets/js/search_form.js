c4d05c7a3dcd406d4b99f6fec4f084fe3890d516
backend <backend@analitika.online>
2019-05-14 14:42:15 +0300

    Initial commit, webpacked!

---
+++ src/assets/js/search_form.js
@@ -0,0 +1,44 @@
+import {$,jQuery} from 'jquery';
+window.$ = $;
+window.jQuery = jQuery;
+
+// search form
+$(document).ready(function(){
+			var searchPopUp = $('.search__popup_invisible');
+			var inputBox = $('.search__input');
+			var isOpen = false;
+			searchPopUp.click(function(){
+				if(isOpen == false){
+					inputBox.addClass('search__input_opened');
+					inputBox.focus();
+					isOpen = true;
+				} else {
+					inputBox.removeClass('search__input_opened');
+					inputBox.focusout();
+				  	inputBox.val('');
+					isOpen = false;
+				}
+			});  
+			 searchPopUp.mouseup(function(){
+					return false;
+				});
+			inputBox.mouseup(function(){
+					return false;
+				});
+			$(document).mouseup(function(){
+					if(isOpen == true){
+						$('.search__popup_invisible').css('display','block');
+						searchPopUp.click();
+					}
+				});
+		});
+			function buttonUp(){
+				var inputVal = $('.search__input').val();
+				inputVal = $.trim(inputVal).length;
+				if( inputVal !== 0){
+					$('.search__popup_invisible').css('display','none');
+				} else {
+					$('.search__input').val('');
+					$('.search__popup_invisible').css('display','block');
+				}
+			}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import 'bootstrap';
+import './assets/styles/sass/main.scss';
+import './assets/js/navigation';
+// import './assets/js/search_form';
